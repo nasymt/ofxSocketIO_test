8a8466126d89ab20a54d36293ee9b150ce4b8a55
mtok <TadashiMatsuoka@MacBook-Pro.local>
2016-03-05 14:57:48 +0900

    hello world(of->node only)

---
+++ src/ofApp.cpp
@@ -0,0 +1,71 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    isConnected = false;
+    address = "http://127.0.0.1:1337";
+    status = "not connected";
+    
+    socketIO.setup(address);
+    ofAddListener(socketIO.notifyEvent, this, &ofApp::gotEvent);
+    
+    ofAddListener(socketIO.connectionEvent, this, &ofApp::onConnection);
+}
+
+void ofApp::onConnection () {
+    isConnected = true;
+    bindEvents();
+}
+
+void ofApp::bindEvents () {
+    std::string serverEventName = "server-event";
+    socketIO.bindEvent(serverEvent, serverEventName);
+    ofAddListener(serverEvent, this, &ofApp::onServerEvent);
+    
+    std::string pingEventName = "ping";
+    socketIO.bindEvent(pingEvent, pingEventName);
+    ofAddListener(pingEvent, this, &ofApp::onPingEvent);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofDrawBitmapStringHighlight(ofApp::status, 20, 20);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    std::string str("emit_from_client");
+    std::string msg("hello!!!");
+    socketIO.emit(str, msg);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::gotEvent(string& name) {
+    status = name;
+}
+
+//--------------------------------------------------------------
+void ofApp::onServerEvent (ofxSocketIOData& data) {
+    ofLogNotice("ofxSocketIO", data.getStringValue("stringData"));
+    ofLogNotice("ofxSocketIO", ofToString(data.getIntValue("intData")));
+    ofLogNotice("ofxSocketIO", ofToString(data.getFloatValue("floatData")));
+    ofLogNotice("ofxSocketIO", ofToString(data.getBoolValue("boolData")));
+}
+
+void ofApp::onPingEvent (ofxSocketIOData& data) {
+    ofLogNotice("ofxSocketIO", "ping");
+    cout<<"ok"<<endl;
+    std::string pong = "pong";
+    std::string param = "foo";
+    socketIO.emit(pong, param);
+}
---
+++ src/ofApp.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofEvents.h"
+#include "ofxSocketIO.h"
+#include "ofxSocketIOData.h"
+
+class ofApp : public ofBaseApp{
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    
+    void gotEvent(std::string& name);
+    void onServerEvent(ofxSocketIOData& data);
+    void onPingEvent(ofxSocketIOData& data);
+    
+    ofxSocketIO socketIO;
+    
+    bool isConnected;
+    void onConnection();
+    void bindEvents();
+    ofEvent<ofxSocketIOData&> serverEvent;
+    ofEvent<ofxSocketIOData&> pingEvent;
+    
+    std::string address;
+    std::string status;
+};
